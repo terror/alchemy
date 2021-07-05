dddef5fe9410a78379b34bb00af5dd2de9c2f6f6
Liam <liam@scalzulli.com>
2021-07-05 08:33:58 -0400

    Add curry.py

---
+++ python/curry.py
@@ -0,0 +1,14 @@
+def curry(func):
+  def wrap(*args, **kwargs):
+    ans = func(*args, **kwargs)
+    def r(*args, **kwargs):
+      return ans + func(*args, **kwargs)
+    return r
+  return wrap
+
+@curry
+def add(x, y):
+  return x + y
+
+if __name__ == '__main__':
+  print(add(1, 2)(1, 2)) # 6
