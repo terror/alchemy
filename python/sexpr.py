5fac2d9e54937d409b5bf27baabb102246fab62c
Liam <liam@scalzulli.com>
2021-07-17 12:39:45 -0400

    Fix bugs in `sexpr.py`

--- python/sexpr.py
+++ python/sexpr.py
@@ -1,12 +1,18 @@
-import sys, argparse, unittest, ast
+import sys, argparse, unittest, ast, string
 from collections import deque
 
 # **** tests ****
 
 class TestParse(unittest.TestCase):
   def setUp(self):
-    self.cases = [('(first (list 1 (+ 2 3) 9))', ["first", ["list", 1, ["+", 2, 3], 9]]),
-                  ('(first (list 1 (+ 2 3.5) 9.4))', ["first", ["list", 1, ["+", 2, 3.5], 9.4]])]
+    self.cases = [
+      ('(first (list 1 (+ 2 3) 9))',            ['first', ['list', 1, ['+', 2, 3], 9]]),
+      ('(first (list 1 (+ 2 3.5) 9.4))',        ['first', ['list', 1, ['+', 2, 3.5], 9.4]]),
+      ('(+ 3 (* 5 4 (/ 7 8)))',                 ['+', 3, ['*', 5, 4, ['/', 7, 8]]]),
+      ('(+ 322 (* 55 488 (/ 78 81)))',          ['+', 322, ['*', 55, 488, ['/', 78, 81]]]),
+      ('(+ 1 (+ 2 3 (+ 4 5 (+ 6 7 (+ 8 9)))))', ['+', 1, ['+', 2, 3, ['+', 4, 5, ['+', 6, 7, ['+', 8, 9]]]]]),
+      ('()',                                    [])
+    ]
 
   def test_parse(self):
     for expr, result in self.cases:
@@ -25,18 +31,20 @@ def parse(expr):
   # str | number -> append to current list
   # (            -> append a list
   # )            -> merge current list with previous
-  # ['first', '(list', '1', '(+', '2', '3)', '9)']
-  expr = deque(expr[1:][:-1].split())
+  # ['+', ' ', '3', ' ', '(', '*', ' ', '5', ' ', '4', ' ', '(', '/', ' ', '7', ' ', '8', ')', ')']
+  expr   = deque(expr[1:][:-1])
   result = [[]]
   while expr:
     curr = expr.popleft()
-    first, last = curr[0], curr[-1]
-    if first == '(':
-      result.append([curr[1:]])
-    elif last == ')':
-      result[-1].append(eval(curr[:-1]))
+    if curr == '(':
+      result.append([])
+    elif curr == ')':
       result[-2].append(result.pop())
     else:
+      if curr == ' ':
+        continue
+      while expr[0] not in ['(', ')', ' ']:
+        curr += expr.popleft()
       result[-1].append(eval(curr))
   return result[0]
 
@@ -50,7 +58,7 @@ def main(args):
     sys.argv = [sys.argv[0]]
     unittest.main()
     return
-  print(parse('(first (list 1 (+ 2 3) 9))'))
+  print(parse('(+ 3 (* 5 4 (/ 7 8)))'))
 
 if __name__ == '__main__':
   main(cli())
